d3f73b8bba58461501f6f91f64ae1bb307f87818
Saidaziz007 <saidaminovvs@gmail.com>
2023-11-19 16:14:53 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,93 @@
+$('.owl-carousel').owlCarousel({
+   center: true,
+   items: 3,
+   loop: true,
+  //  margin: 10,
+   nav: true,
+   dots: true,
+   margin: 30,
+   navText: ['<img src="images/prev.svg"/>', '<img src="images/next.svg"/>'],
+   autoplay: true,
+   responsive:{
+      0: {
+         items: 1,
+       },
+       600: {
+         items: 2,
+       },
+       800: {
+         items: 3,
+       },
+   }
+});
+
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+
+
+AOS.init();
+
+
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-100%";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+
+
+const loading = document.getElementById("loading");
+
+const loadingDuration = 2000;
+
+setTimeout(() => {
+  loading.classList.add('loading-none');
+}, loadingDuration);
+
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
